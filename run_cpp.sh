fd6715315f811a72c13068280c1f34fe6a4907cf
lehuyduc <lehuyduc3@gmail.com>
2024-01-05 20:22:55 +0700

    Remove ffast-math because it's unnecessary

--- run_cpp.sh
+++ run_cpp.sh
@@ -1,4 +1,4 @@
 rm main
-g++ -o main 1brc_final.cpp -O3 -std=c++17 -march=native -mavx2 -m64 -ffast-math
+g++ -o main 1brc_final.cpp -O3 -std=c++17 -march=native -mavx2 -m64
 #g++ -S -o main.s 1brc_final.cpp -O3 -std=c++17 -march=native -mavx2  -fverbose-asm -g
 time ./main

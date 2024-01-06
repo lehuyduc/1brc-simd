bc92895516d3a3be131e028e6fb8f689852ecaf7
lehuyduc <lehuyduc3@gmail.com>
2024-01-06 11:38:03 +0700

    Update run script

--- run_cpp.sh
+++ run_cpp.sh
@@ -1,4 +1,4 @@
 rm main
-g++ -o main 1brc_final.cpp -O3 -std=c++17 -march=native -mavx2 -m64
-#g++ -S -o main.s 1brc_final.cpp -O3 -std=c++17 -march=native -mavx2  -fverbose-asm -g
-time ./main
+g++ -o main 1brc_final_valid.cpp -O3 -std=c++17 -march=native -m64
+#g++ -S -o main.s 1brc_final_valid.cpp -O3 -std=c++17 -march=native -fverbose-asm -g
+time ./main measurements.txt
